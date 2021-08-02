351613fc1beef023ec2aa1491854b5a4eaabeaae
root <root@raspberrypi>
2021-08-02 16:22:36 +0100

    bluetooth menu status config added

--- oled/windows/headphone.py
+++ oled/windows/headphone.py
@@ -13,37 +13,49 @@ class Headphonemenu(WindowBase):
     def __init__(self, windowmanager):
         super().__init__(windowmanager)
         self.counter = 0
+        self.descr = []
+        self.descr.append("Zurück")
+        self.descr.append(settings.NAME_DEV_BT_1 + " " + settings.ENABLED_DEV_BT_1)
+        self.descr.append(settings.NAME_DEV_BT_2 + " " + settings.ENABLED_DEV_BT_2)
+        self.descr.append("Lautsprecher")
+
 
     def render(self):
         with canvas(self.device) as draw:
+            mwidth = Headphonemenu.font.getsize(self.descr[self.counter])
+            draw.text((64 - int(mwidth[0]/2),1), text=self.descr[self.counter], font=Headphonemenu.font, fill="white")
+
             x_coord = 2 + self.counter * 30
-            y_coord = 7
+            y_coord = 27
 
             draw.rectangle((x_coord, y_coord, x_coord+20, y_coord+30), outline=255, fill=0)
 
 #            draw.text((13, 25), text="Zurück", font=Headphonemenu.font, fill="white")
-            draw.text((5, 15), text="\uf0a8", font=Headphonemenu.faicons, fill="white")
-
+            draw.text((5, 35), text="\uf0a8", font=Headphonemenu.faicons, fill="white")
+            text = "\uf293" if settings.ENABLED_DEV_BT_1 == "ON" else "\uf294"
 #            draw.text((30, 25), text="Bluetooth 1", font=Headphonemenu.font, fill="white")
-            draw.text((35, 15), text="\uf293", font=Headphonemenu.faicons, fill="white")
+            draw.text((35, 35), text=text, font=Headphonemenu.faicons, fill="white")
 
+            text = "\uf293" if settings.ENABLED_DEV_BT_2 == "ON" else "\uf294"
 #            draw.text((30, 25), text="Kopfhörer", font=Headphonemenu.font, fill="white")
-            draw.text((65, 15), text="\uf293", font=Headphonemenu.faicons, fill="white")
+            draw.text((65, 35), text=text, font=Headphonemenu.faicons, fill="white")
 
 #            draw.text((56, 25), text="Ja", font=Headphonemenu.font, fill="white")
-            draw.text((95, 15), text="\uf028", font=Headphonemenu.faicons, fill="white")
+            draw.text((95, 35), text="\uf028", font=Headphonemenu.faicons, fill="white")
 
 
     def push_callback(self,lp=False):
         if self.counter == 0:
             self.windowmanager.set_window("mainmenu")
-        elif self.counter == 1:
+        elif self.counter == 1 and settings.ENABLED_DEV_BT_1 == "ON":
             integrations.bluetooth.enable_dev_bt_1()
-        elif self.counter == 2:
+            self.windowmanager.set_window("mainmenu")    
+        elif self.counter == 2 and settings.ENABLED_DEV_BT_1 == "ON":
             integrations.bluetooth.enable_dev_bt_2()
+            self.windowmanager.set_window("mainmenu")
         elif self.counter == 3:
             integrations.bluetooth.enable_dev_local()
-        self.windowmanager.set_window("mainmenu")
+            self.windowmanager.set_window("mainmenu")
 
     def turn_callback(self, direction, key=None):
         if key:
