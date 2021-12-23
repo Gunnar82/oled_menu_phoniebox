b47a08c8bdc173a22ddad917e65d0fe6d9f136c1
gunnar_82 <gunnar_82@hotmail.com>
2021-12-23 21:58:23 +0100

    added sudo for mpg123 in usb handling - todo

--- usr/local/sbin/pb_import.sh
+++ usr/local/sbin/pb_import.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 DATE=`date +"%d/%m/%Y %H:%M:%S"`
 DEVICE=$1
 MOUNTPATH="/media/pb_import/$DEVICE/"
@@ -12,27 +13,27 @@ DESTPATH="/home/pi/RPi-Jukebox-RFID/shared/audiofolders/"
 mkdir -p $MOUNTPATH
 mount /dev/$DEVICE $MOUNTPATH -oro
 
-/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
+sudo /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
 
 if [ -f "$IMPORTFILE" ]; then
-    /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
+    sudo /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
 
     rsync -ru --size-only $MOUNTPATH $DESTPATH
     RET=$?
     if [ $RET -eq 0 ] ; then
 	chown -R pi.pi $DESTPATH
-	/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
-	/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
-	/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
+	sudo /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
+	sudo /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
+	sudo /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
 	RV=0
     else
-	/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-03.mp3
-	/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-03.mp3
-	/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-03.mp3
+	sudo /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-03.mp3
+	sudo /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-03.mp3
+	sudo /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-03.mp3
 	RV=1
     fi
 else
-    /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-05.mp3
+    sudo /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-05.mp3
     RV=2
 fi
 
