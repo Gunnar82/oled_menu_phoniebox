3b2158957aac776d7a073fe335b24dbf7c05c8aa
gunnar <gunnar_82@hotmail.com>
2021-12-23 19:13:07 +0100

    added some new features

---
+++ usr/local/sbin/pb_import.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DATE=`date +"%d/%m/%Y %H:%M:%S"`
+DEVICE=$1
+MOUNTPATH="/media/$DEVICE/"
+IMPORTFILE="$MOUNTPATH/PB_import.txt"
+DESTPATH="/home/pi/RPi-Jukebox-RFID/shared/audiofolders/"
+
+
+
+
+mkdir $MOUNTPATH
+mount /dev/$DEVICE $MOUNTPATH -oro
+
+    /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
+    /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
+
+
+if [ -f "$IMPORTFILE" ]; then
+    rsync -ru --size-only $MOUNTPATH $DESTPATH
+    RET=$?
+    if [ $RET -eq 0 ] ; then
+	chown -R pi.pi $DESTPATH
+	/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
+	/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
+	/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-07.mp3
+	RV=0
+    else
+	/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-03.mp3
+	/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-03.mp3
+	/usr/bin/mpg123 /home/pi/oledctrl/audio/beep-03.mp3
+	RV=1
+    fi
+else
+    /usr/bin/mpg123 /home/pi/oledctrl/audio/beep-05.mp3
+    RV=2
+fi
+
+
+umount $MOUNTPATH
+exit $RV
