824738c6a03c5ba90719f5f6e3dd83e9c9ef4df9
gunnar_82 <gunnar_82@hotmail.com>
2023-07-23 20:04:43 +0200

    bufgixing listbase layout

--- oled/integrations/display/st7789.py
+++ oled/integrations/display/st7789.py
@@ -33,7 +33,7 @@ def set_fonts():
     settings.FONT_HEIGHT_XXL = 40
     settings.FONT_HEIGHT_XL = 30
     settings.FONT_HEIGHT_NORMAL = 24
-    settings.FONT_HEIGHT_SMALL = 18
+    settings.FONT_HEIGHT_SMALL = 20
 
     settings.DISPLAY_WIDTH = 240
     settings.DISPLAY_HEIGHT = 240 # or 64
