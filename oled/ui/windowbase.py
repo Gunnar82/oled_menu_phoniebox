60a928b475d8f77ea40843e623ac760b827119a3
Gunnar <gunnar_82@hotmail.com>
2023-10-30 12:18:16 +0100

    busy render -  layout chaning

--- oled/ui/windowbase.py
+++ oled/ui/windowbase.py
@@ -6,7 +6,7 @@ from PIL import ImageFont
 from luma.core.render import canvas
 
 
-busyfont = ImageFont.truetype(settings.FONT_TEXT, size=settings.FONT_SIZE_L)
+busyfont = ImageFont.truetype(settings.FONT_TEXT, size=settings.FONT_SIZE_NORMAL)
 busyfaicons = ImageFont.truetype(settings.FONT_ICONS, size=settings.FONT_SIZE_L)
 busyfaiconsbig = ImageFont.truetype(settings.FONT_ICONS, size=settings.FONT_SIZE_XXXL)
 
@@ -35,8 +35,17 @@ class WindowBase():
     def set_busy(self,busytext1,busysymbol=symbols.SYMBOL_SANDCLOCK,busytext2="", busyrendertime=3,busytext3=""):
         self.busytext1 = busytext1
         self.busysymbol = busysymbol
-        self.busytext2 = busytext2
+        
         self.busytext3 = busytext3
+        if busyfont.getsize(busytext2)[0] > settings.DISPLAY_WIDTH:
+            pos = len(busytext2) // 2
+            self.busytext4 = busytext2[:pos]
+            self.busytext2 = busytext2[pos:]
+
+        else:
+            self.busytext4 = ""
+            self.busytext2 = busytext2
+
         self.busyrendertime = busyrendertime
         self.busy = True
 
@@ -53,6 +62,10 @@ class WindowBase():
             if (self.busytext2 != ""):
                 mwidth,mheight = busyfont.getsize(self.busytext2)
                 draw.text(((settings.DISPLAY_WIDTH - mwidth) / 2, settings.DISPLAY_HEIGHT - mheight - 2), text=self.busytext2, font=busyfont, fill=textcolor2) #sanduhr
+
+            if (self.busytext4 != ""):
+                mwidth,mheight = busyfont.getsize(self.busytext4)
+                draw.text(((settings.DISPLAY_WIDTH - mwidth) / 2, settings.DISPLAY_HEIGHT - 2 * (mheight + 2)), text=self.busytext4, font=busyfont, fill=textcolor2) #sanduhr
 
             mwidth,mheight = busyfaiconsbig.getsize(self.busysymbol)
             draw.text(((settings.DISPLAY_WIDTH - mwidth) / 2, (settings.DISPLAY_HEIGHT - mheight) / 2), text=self.busysymbol, font=busyfaiconsbig, fill=symbolcolor) #sanduhr
