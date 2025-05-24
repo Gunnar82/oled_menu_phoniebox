21ff1a95a6d17b7e4ec7f0ffa953ac620f48b325
gunnar_82 <gunnar_82@hotmail.com>
2025-05-24 21:44:17 +0200

    leds überarbeitet

--- oled/integrations/outputs/mcp_23017_leds.py
+++ oled/integrations/outputs/mcp_23017_leds.py
@@ -22,35 +22,43 @@ class mcp_23017_leds:
     i2c = smbus.SMBus(1) 
 
 
-    def get_led_value_from_value(self, value, only_current = True):
+    def get_led_value_from_value(self, value, only_current = True, blink_value = False):
         base_value = 0
         # Bestimmen des Basis-Rückgabewerts basierend auf dem `value`
         for threshold, return_value in self.config.value_thresholds:
             if value >= threshold:
                 base_value = return_value
+                base_index = threshold
                 break
 
-        if only_current: return base_value
+        if only_current:
+            if blink_value and isinstance(base_value,list):
+                self.blink_value = not self.blink_value
+
+                return (base_value[0] if not self.blink_value else base_value[1])
+            else:
+                self.blink_value = False
+
+                return (base_value[0] if isinstance(base_value,list) else base_value)
 
         # Bestimmen des abwechselnden Rückgabewerts basierend auf dem Aufruf
-        result_value = self.decrements[self.toggle_index]
+        result_value = 127 - self.config.value_thresholds[self.toggle_index][1]
 
         # Erhöhe den Index für den nächsten Funktionsaufruf
         self.toggle_index += 1
 
         # Wenn das Ende der Decrements-Liste erreicht ist, zurück auf den ersten Wert
-        if self.decrements[self.toggle_index] > base_value:
+        if 100 - self.config.value_thresholds[self.toggle_index][0] > base_value:
             self.toggle_index = 0
 
         # Multiplizieren des `base_value` mit dem abwechselnden Wert
-        return self.decrements[self.toggle_index]
+        return (self.config.value_thresholds[self.toggle_index][1][0] if isinstance(self.config.value_thresholds[self.toggle_index][1],list) else self.config.value_thresholds[self.toggle_index])
 
     async def set(self):
 
         self.i2c.write_byte_data(self.config.ADDR, self.config.IOADDR, 0x00) # IODIRB
 
         self.toggle_index = 0  # Steuert den Rückgabewert, beginnt bei 0
-        self.decrements = [0, 1, 3, 7, 15, 31, 63, 127]  # 2er Potenzen minus 1
 
 
         pos = 0
@@ -69,7 +77,7 @@ class mcp_23017_leds:
                     if ( settings.job_i <= settings.job_t or settings.job_t == -1) and settings.job_i > -1: 
 
                         percent  = int(settings.job_i / (self.usersettings.IDLE_POWEROFF * 60) * 100)
-                        value = self.get_led_value_from_value(percent) + 128 #
+                        value = 127 - self.get_led_value_from_value(percent,blink_value = True)
 
                     else:
 
@@ -77,13 +85,13 @@ class mcp_23017_leds:
                         seconds_till_shutdown = int(self.usersettings.shutdowntime - time.monotonic())
                         total_seconds_for_shutdown = int(self.usersettings.shutdowntime - self.usersettings.shutdownset)
                         percent  = int((seconds_till_shutdown) /  total_seconds_for_shutdown * 100)
-                        value = 255 - self.get_led_value_from_value(percent) #
+                        value = self.get_led_value_from_value(percent, blink_value = True ) #
 
                 elif pos == 0 and not "x728" in settings.INPUTS: # KEIN Timeout
                     pos == 1
 
                 elif "x728"in settings.INPUTS  and pos == 1:
-                    value = self.get_led_value_from_value(settings.battcapacity, only_current = not settings.battloading)
+                    value = self.get_led_value_from_value(settings.battcapacity, only_current = not settings.battloading, blink_value = True) + 128
 
                 elif pos == 1:
                     pos = 2
@@ -104,11 +112,9 @@ class mcp_23017_leds:
         self.config = config
         self.usersettings = usersettings
         settings.mcp_leds_change = False
+        self.blink_value = False
 
         self.toggle_index = 0  # Steuert, welcher Decrement verwendet wird, beginnt bei 0
-        self.decrements = [1, 2, 4, 8, 16, 32, 64]  # Liste der Decrements (Potenzen von 2)
-        self.thresholds = [94, 85, 75, 60, 50, 30, 15]  # Schwellenwerte
-
         self.loop.create_task(self.set())
 
 
